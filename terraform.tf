ab76a6a44dcbbf92e931785e9dbdb91145f71322
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2021-03-22 20:31:29 +0100

    feat: initial commit

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    github = {
+      source = "hashicorp/github"
+    }
+
+    null = {
+      source = "hashicorp/null"
+    }
+
+    external = {
+      source = "hashicorp/external"
+    }
+  }
+}
